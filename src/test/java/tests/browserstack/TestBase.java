65a9217d2dcecabe99aa2072b5c085e268bc7ba1
grad0ff <grad0ff@mail.ru>
2022-08-29 22:31:29 +0300

    added pattern files

---
+++ src/test/java/tests/browserstack/TestBase.java
@@ -0,0 +1,37 @@
+package tests.browserstack;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import drivers.BrowserstackMobileDriver;
+import drivers.LocalMobileDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static io.qameta.allure.Allure.step;
+
+public class TestBase {
+
+    @BeforeAll
+    public static void beforeAll() {
+        Configuration.browser = LocalMobileDriver.class.getName();
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        step("Close driver", Selenide::closeWebDriver);
+    }
+}
